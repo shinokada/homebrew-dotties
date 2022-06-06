0544a29c1e3e104dbcff6d465c33731a33bc0319
Shinichi Okada <okada.shin@gmail.com>
2022-06-06 15:02:53 +0900

    version v0.3.0

--- dotties.rb
+++ dotties.rb
@@ -1,8 +1,8 @@
 class Dotties < Formula
     desc "Dotties is a simple dotfile management script. It saves specified dotfiles in the home directory to a Gist and clone them from the Gist."
     homepage "https://github.com/shinokada/dotties"
-    url "https://github.com/shinokada/dotties/archive/refs/tags/v0.2.6.tar.gz"
-    sha256 "424272f626d6ec3b0cf24a180df2a598d673f0107a516db5da136ad10c35d5a9"
+    url "https://github.com/shinokada/dotties/archive/refs/tags/v0.3.0.tar.gz"
+    sha256 "0b2e7fcf4b7c361326ed312617e53b0e3e1f81b8b05170ff4f3de3bc4359db08"
     license "MIT"
   
     depends_on "gh"
